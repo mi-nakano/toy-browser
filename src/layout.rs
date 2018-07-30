17b2c723dce09e194e12d5fa97b6f62521f61de2
mi-nakano <mizuki590@gmail.com>
2018-07-30 23:22:15 +0900

    impl layout module

--- src/layout.rs
+++ src/layout.rs
@@ -64,6 +64,146 @@ impl LayoutBox {
         }
     }
 
+    fn layout(&mut self, containing_block: Dimensions) {
+        match self.box_type {
+            BlockNode(_) => self.layout_block(containing_block),
+            InlineNode(_) => {} // TODO
+            AnonymousBlock => {} // TODO
+        }
+    }
+
+    fn layout_block(&mut self, containing_block: Dimensions) {
+        // Child width can depend on parent width, so we need to calculate
+        // this box's width before laying out its children.
+        self.calculate_block_width(containing_block);
+
+        // Determine where the box is located within its container.
+        self.calculate_block_position(containing_block);
+
+        // Recursively lay out the children of this box.
+        self.layout_block_children();
+
+        // Parent height can depend on child height, so `calculate_height`
+        // must be called *after* the children are laid out.
+        self.calculate_block_height();
+    }
+
+    fn calculate_block_width(&mut self, containing_block: Dimensions) {
+        let style = self.get_style_node();
+
+        // `width` has initial value `auto`.
+        let auto = Keyword("auto".to_string());
+        let mut width = style.value("width").unwrap_or(auto.clone());
+
+        // margin, border, and padding have initial value 0.
+        let zero = Length(0.0, Px);
+
+        let mut margin_left = style.lookup("margin-left", "margin", &zero);
+        let mut margin_right = style.lookup("margin-right", "margin", &zero);
+
+        let border_left = style.lookup("border-left-width", "border-width", &zero);
+        let border_right = style.lookup("border-right-width", "border-width", &zero);
+
+        let padding_left = style.lookup("padding-left", "padding", &zero);
+        let padding_right = style.lookup("padding-right", "padding", &zero);
+
+        let total = sum([&margin_left, &margin_right, &border_left, &border_right,
+                        &padding_left, &padding_right, &width].iter().map(|v| v.to_px()));
+
+        // If width is not auto and the total is wider than the container, treat auto margins as 0.
+        if width != auto && total > containing_block.content.width {
+            if margin_left == auto {
+                margin_left = Length(0.0, Px);
+            }
+            if margin_right == auto {
+                margin_right = Length(0.0, Px);
+            }
+        }
+
+        // Adjust used values so that the above sum equals `containing_block.width`.
+        // Each arm of the `match` should increase the total width by exactly `underflow`,
+        // and afterward all values should be absolute lengths in px.
+        let underflow = containing_block.content.width - total;
+
+        match (width == auto, margin_left == auto, margin_right == auto) {
+            // If the values are overconstrained, calculate margin_right.
+            (false, false, false) => {
+                margin_right = Length(margin_right.to_px() + underflow, Px);
+            }
+
+            // If exactly one size is auto, its used value follows from the equality.
+            (false, false, true) => { margin_right = Length(underflow, Px); }
+            (false, true, false) => { margin_left  = Length(underflow, Px); }
+
+            // If width is set to auto, any other auto values become 0.
+            (true, _, _) => {
+                if margin_left == auto { margin_left = Length(0.0, Px); }
+                if margin_right == auto { margin_right = Length(0.0, Px); }
+
+                if underflow >= 0.0 {
+                    // Expand width to fill the underflow.
+                    width = Length(underflow, Px);
+                } else {
+                    // Width can't be negative. Adjust the right margin instead.
+                    width = Length(0.0, Px);
+                    margin_right = Length(margin_right.to_px() + underflow, Px);
+                }
+            }
+
+            // If margin-left and margin-right are both auto, their used values are equal.
+            (false, true, true) => {
+                margin_left = Length(underflow / 2.0, Px);
+                margin_right = Length(underflow / 2.0, Px);
+            }
+        }
+
+        let d = &mut self.dimensions;
+        d.content.width = width.to_px();
+
+        d.padding.left = padding_left.to_px();
+        d.padding.right = padding_right.to_px();
+
+        d.border.left = border_left.to_px();
+        d.border.right = border_right.to_px();
+
+        d.margin.left = margin_left.to_px();
+        d.margin.right = margin_right.to_px();
+    }
+
+    fn calculate_block_position(&mut self, containing_block: Dimensions) {
+        let style = self.get_style_node();
+        let d = &mut self.dimensions;
+
+        // margin, border, and padding have initial value 0.
+        let zero = Length(0.0, Px);
+
+        // If margin-top or margin-bottom is `auto`, the used value is zero.
+        d.margin.top = style.lookup("margin-top", "margin", &zero).to_px();
+        d.margin.bottom = style.lookup("margin-bottom", "margin", &zero).to_px();
+
+        d.border.top = style.lookup("border-top-width", "border-width", &zero).to_px();
+        d.border.bottom = style.lookup("border-bottom-width", "border-width", &zero).to_px();
+
+        d.padding.top = style.lookup("padding-top", "padding", &zero).to_px();
+        d.padding.bottom = style.lookup("padding-bottom", "padding", &zero).to_px();
+
+        d.content.x = containing_block.content.x +
+            d.margin.left + d.border.left + d.padding.left;
+
+        // Position the box below all the previous boxes in the container.
+        d.content.y = containing_block.content.height + containing_block.content.y +
+            d.margin.top + d.border.top + d.padding.top;
+    }
+
+    fn layout_block_children(&mut self) {
+        let d = &mut self.dimensions;
+        for child in &mut self.children {
+            child.layout(*d);
+            // Track the height so each child is laid out below the previous content.
+            d.content.height = d.content.height + child.dimensions.margin_box().height;
+        }
+    }
+
     fn get_inline_container(&mut self) -> &mut LayoutBox {
         match self.box_type {
             InlineNode(_) | AnonymousBlock => self,
@@ -79,4 +219,3 @@ impl LayoutBox {
         }
     }
 }
-
